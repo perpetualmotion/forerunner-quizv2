0dc0765385b8f37c19811524a8ca3954dd2a263d
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-01-11 12:18:03 -0500

    Abstracted branching logic

--- script.js
+++ script.js
@@ -93,72 +93,104 @@ $("[data-clear-target]").on("click", function () {
 
 //Branching Logic ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//j6 is hidden if j5 answer is j5.1 or j5.8
-//does j5 need a value of data-question for this logic to work?
-$("[data-question]").on("click", function () {
-  if ($(this).attr("data-question") === "q30") {
-    if ($(this).attr("data-answer") === "j5-1" || "j5-8") {
-      //make section j6 not visible
-      document.getElementById("j6").style.visibility = "hidden";
-    }
-  }
-});
-
-//j7 is hidden if j3 is not j3.4 OR if j3 is j3.6 or j3.7
-$("[data-question]").on("click", function () {
-  if ($(this).attr("data-question") === "q12") {
-    if ($(this).attr("data-answer") !== "j3-4") {
-      //make section j7 not visible
-      $("[label=3 of 8]").hide();
-    }
-    if ($(this).attr("data-answer") === "j3-6" || "j3-7") {
-      //make section j7 not visible
-      //hides full page as there is only one question
-      $("[label=3 of 8]").hide();
-    }
-  }
-});
-
-//j8 is hidden if j3 is not 3.1, 3.2, 3.3 or 3.5
-$("[data-question]").on("click", function () {
-  if ($(this).attr("data-question") === "q12") {
-    if ($(this).attr("data-answer") !== "j3-1" || "j3-2" || "j3-3" || "j3-5") {
-      //make section j8 not visible
-      document.getElementById("j8").style.visibility = "hidden";
-    }
-  }
-});
-
-//j9 is hidden if j3 is not 3.1, 3.2, 3.3, or 3.5
-$("[data-question]").on("click", function () {
-  if ($(this).attr("data-question") === "q12") {
-    if ($(this).attr("data-answer") !== "j3-1" || "j3-2" || "j3-3" || "j3-5") {
-      //make section j9 not visible
-      document.getElementById("j9").style.visibility = "hidden";
-    }
-  }
-});
-
-//j10 is hidden if j3 is not j3.4
-$("[data-question]").on("click", function () {
-  if ($(this).attr("data-question") === "q12") {
-    if ($(this).attr("data-answer") !== "j3-4") {
-      //make section j10 not visible
-      document.getElementById("j10").style.visibility = "hidden";
-    }
-  }
-});
-
-//Page 4 is hidden if j3 answer is j3.6 or j3.7
-//Can't hide questions individually need to hide the full page
+//Hide j6
 $(
-  "[data-question]".on("click", function () {
-    if ($(this).attr("data-question") === "q12") {
-      if ($(this).attr("data-answer") === "j3.6" || "j3.7") {
-        $("[label=4 of 8]").hide();
-      }
+  "[hide-question-6]".on("click", function () {
+    if ($(this).attr("hide-question-6") == true) {
+      document.getElementById("j6").hide();
+    } else {
+      //nothing
+    }
+  })
+);
+//Hide j7 + 710
+$(
+  "[hide-question-710]".on("click", function () {
+    if ($(this.attr("hide-question-710") == true)) {
+      document.getElementById("j7").hide();
+      document.getElementById("j10").hide();
+    }
+  })
+);
+//Hide j8 + j9
+$(
+  "[hide-question-89]".on("click", function () {
+    if ($(this.attr("hide-question-89") == true)) {
+      document.getElementById("j8").hide();
+      document.getElementById("j9").hide();
     }
   })
 );
 
-//Input type questions have maxlimit of 24 characters
+//Input type questions have characters between 0-24
+//^(0?[0-9]|1[0-9]|2[0-4])$
+$(
+  "[data-type=input]".on("onfocusout", function () {
+    //validate if regex is true or not
+    //toggle webflow error field attr
+    //cannot navigate to next page
+  })
+);
+
+//Select up to 5 option
+
+//j6 is hidden if j5 answer is j5.1 or j5.8
+//does j5 need a value of data-question for this logic to work?
+// $("[data-question]").on("click", function () {
+//   if ($(this).attr("data-question") === "q30") {
+//     if ($(this).attr("data-answer") === "j5-1" || "j5-8") {
+//       document.getElementById("j6").style.visibility = "hidden";
+//     }
+//   }
+// });
+
+// //j7 is hidden if j3 is not j3.4 OR if j3 is j3.6 or j3.7
+// $("[data-question]").on("click", function () {
+//   if ($(this).attr("data-question") === "q12") {
+//     if ($(this).attr("data-answer") !== "j3-4") {
+//       $("[label=3 of 8]").hide();
+//     }
+//     if ($(this).attr("data-answer") === "j3-6" || "j3-7") {
+//       $("[label=3 of 8]").hide();
+//     }
+//   }
+// });
+
+// //j8 is hidden if j3 is not 3.1, 3.2, 3.3 or 3.5
+// $("[data-question]").on("click", function () {
+//   if ($(this).attr("data-question") === "q12") {
+//     if ($(this).attr("data-answer") !== "j3-1" || "j3-2" || "j3-3" || "j3-5") {
+//       document.getElementById("j8").style.visibility = "hidden";
+//     }
+//   }
+// });
+
+// //j9 is hidden if j3 is not 3.1, 3.2, 3.3, or 3.5
+// $("[data-question]").on("click", function () {
+//   if ($(this).attr("data-question") === "q12") {
+//     if ($(this).attr("data-answer") !== "j3-1" || "j3-2" || "j3-3" || "j3-5") {
+//       document.getElementById("j9").style.visibility = "hidden";
+//     }
+//   }
+// });
+
+// //j10 is hidden if j3 is not j3.4
+// $("[data-question]").on("click", function () {
+//   if ($(this).attr("data-question") === "q12") {
+//     if ($(this).attr("data-answer") !== "j3-4") {
+//       document.getElementById("j10").style.visibility = "hidden";
+//     }
+//   }
+// });
+
+// //Page 4 is hidden if j3 answer is j3.6 or j3.7
+// //Can't hide questions individually need to hide the full page
+// $(
+//   "[data-question]".on("click", function () {
+//     if ($(this).attr("data-question") === "q12") {
+//       if ($(this).attr("data-answer") === "j3.6" || "j3.7") {
+//         $("[label=4 of 8]").hide();
+//       }
+//     }
+//   })
+// );
